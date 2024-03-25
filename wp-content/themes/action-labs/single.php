b48a6bdeb1ab2274409043d7ff9de54d19167f1d
Jonathan Carvalho <jonathan.carvalho1807@gmail.com>
2024-03-25 12:24:09 -0300

    feat: Created blog page html structure

--- wp-content/themes/action-labs/single.php
+++ wp-content/themes/action-labs/single.php
@@ -1,39 +1,33 @@
 <?php
+
 /**
  * The template for displaying all single posts
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
- * @package Action_Labs
+ * @package action-labs
  */
 
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
+	<div class="hero">
+		<div class="overlay"></div>
+		<?= the_post_thumbnail('full'); ?>
+	</div>
 
+	<div class="container content">
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'action-labs' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'action-labs' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
+			get_template_part('template-parts/content', 'single-post');
+		endwhile;
 		?>
+	</div>
 
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_sidebar();
