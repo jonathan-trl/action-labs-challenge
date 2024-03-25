863e7d5518c4cfd813c99bf522928cd67b01fc04
Jonathan Carvalho <jonathan.carvalho1807@gmail.com>
2024-03-25 12:22:27 -0300

    feat: Added functionality to the theme

--- wp-content/themes/action-labs/functions.php
+++ wp-content/themes/action-labs/functions.php
@@ -1,15 +1,16 @@
 <?php
+
 /**
- * Action Labs functions and definitions
+ * action-labs functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package Action_Labs
+ * @package action-labs
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,17 +20,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function action_labs_setup() {
+function action_labs_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on Action Labs, use a find and replace
+		* If you're building a theme based on action-labs, use a find and replace
 		* to change 'action-labs' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'action-labs', get_template_directory() . '/languages' );
+	load_theme_textdomain('action-labs', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,19 +39,19 @@ function action_labs_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'action-labs' ),
+			'menu-1' => esc_html__('Primary', 'action-labs'),
 		)
 	);
 
@@ -83,7 +85,7 @@ function action_labs_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -100,7 +102,7 @@ function action_labs_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'action_labs_setup' );
+add_action('after_setup_theme', 'action_labs_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +111,24 @@ add_action( 'after_setup_theme', 'action_labs_setup' );
  *
  * @global int $content_width
  */
-function action_labs_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'action_labs_content_width', 640 );
+function action_labs_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('action_labs_content_width', 640);
 }
-add_action( 'after_setup_theme', 'action_labs_content_width', 0 );
+add_action('after_setup_theme', 'action_labs_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function action_labs_widgets_init() {
+function action_labs_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'action-labs' ),
+			'name'          => esc_html__('Sidebar', 'action-labs'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'action-labs' ),
+			'description'   => esc_html__('Add widgets here.', 'action-labs'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -132,22 +136,23 @@ function action_labs_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'action_labs_widgets_init' );
+add_action('widgets_init', 'action_labs_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function action_labs_scripts() {
-	wp_enqueue_style( 'action-labs-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'action-labs-style', 'rtl', 'replace' );
+function action_labs_scripts()
+{
+	wp_enqueue_style('action-labs-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('action-labs-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'action-labs-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('action-labs-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'action_labs_scripts' );
+add_action('wp_enqueue_scripts', 'action_labs_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -172,7 +177,44 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+add_post_type_support('page', 'excerpt');
+
+function remove_menus()
+{
+	remove_menu_page('edit-comments.php');
+}
+
+add_action('admin_menu', 'remove_menus');
+
+function disable_media_comment($open, $post_id)
+{
+	$post = get_post($post_id);
+	if ($post->post_type == 'attachment') {
+		return false;
+	}
+	return $open;
+}
+
+add_filter('comments_open', 'disable_media_comment', 10, 2);
+
+function actions_labs_sidebars()
+{
+	$args = array(
+		'name'          => 'Footer',
+		'id'            => 'sidebar-footer',
+		'description'   => 'Add widgets here.',
+		'class'         => '',
+		'before_widget' => '<li id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</li>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>'
+	);
+
+	register_sidebars(1, $args);
+}
+
+add_action('widgets_init', 'actions_labs_sidebars');
